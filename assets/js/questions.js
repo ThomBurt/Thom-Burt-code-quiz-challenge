235b505287c5cfe517d65140551fc01bca15d414
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2021-12-18 19:12:43 -0500

    add questions JS

---
+++ assets/js/questions.js
@@ -0,0 +1,102 @@
+// Page 1 - Start Quiz
+
+
+// Question 1
+// Commonly used data types DO NOT include:
+// 1. Strings
+// 2. Booleans
+// 3. Alerts
+// 4. Numbers
+
+
+// Question 2
+// The condition in an if / else statement is enclosed with _________.
+// 1. Quotes
+// 2. Curly Brackets
+// 3. Parenthesis
+// 4. Square Brackets
+
+
+// Question 3
+// Arrays in JavaScript can be used to store _______.
+// 1. Numbers and Strings
+// 2. Other arrays
+// 3. Booleans
+// 4. All of the above
+
+
+// Question 4
+// String values must be enclosed within ______ when bring assigned to variables.
+// 1. Commas
+// 2. Curly Brackets
+// 3. Quotes
+// 4. Parenthesis
+
+
+// Question 5
+// A very useful tool used during development and debugging for printing content to the debugger is:
+// 1. JavaScript
+// 2. Terminal/Bash
+// 3. For Loops
+// 4. console.log
+
+
+// technique from https://www.youtube.com/watch?v=WUBhpSRS_fk&t=0s
+
+let questions = [
+  {
+    numb: 1,
+    question: "Commonly used data types DO NOT include:",
+    answer: "3. Alerts",
+    options: [
+        "1. Strings",
+        "2. Booleans",
+        "3. Alerts",
+        "4. Numbers"
+    ]
+  },
+  {
+    numb: 2,
+    question: "The condition in an if / else statement is enclosed with _________.",
+    answer: "3. Parenthesis",
+    options: [
+        "1. Quotes",
+        "2. Curly Brackets",
+        "3. Parenthesis",
+        "4. Square Brackets"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Arrays in JavaScript can be used to store _______.",
+    answer:  "4. All of the above",
+    options: [
+        "1. Numbers and Strings",
+        "2. Other arrays",
+        "3. Booleans",
+        "4. All of the above"
+    ]
+  },
+  {
+    numb: 4,
+    question: "String values must be enclosed within ______ when bring assigned to variables.",
+    answer: "3. Quotes",
+    options: [
+        "1. Commas",
+        "2. Curly Brackets",
+        "3. Quotes",
+        "4. Parenthesis"
+    ]
+  },
+  {
+    numb: 5,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "4. console.log",
+    options: [
+        "1. JavaScript",
+        "2. Terminal/Bash",
+        "3. For Loops",
+        "4. console.log"
+    ]
+  }
+];
