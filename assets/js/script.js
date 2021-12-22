0cb683aa4f39a40ef31201c0312095dc1d150583
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2021-12-22 16:38:44 -0500

    questions and answers showing correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 var questionsEl = document.querySelector("#questions");
 var paragraph1 = document.querySelector("#paragraph-pg1");
 var optionsEl = document.querySelector("#options");
-var option1 = document.querySelector('#option1')
-var option2 = document.querySelector('#option2')
-var option3 = document.querySelector('#option3')
-var option4 = document.querySelector('#option4')
 
-var correctPopup = document.querySelector('#correct')
-var wrongPopup = document.querySelector('#wrong')
+// var score = timeSecond;
+
+
+// var correctPopup = document.querySelector('#correct')
+// var wrongPopup = document.querySelector('#wrong')
 
 
 //sound fx
@@ -33,7 +32,6 @@ startButton.addEventListener("click", function() {
   });
 
 
-
   //START GAME BUTTON PRESS
   let currentQuestionIndex = 0;
 
@@ -44,9 +42,17 @@ startButton.addEventListener("click", function() {
     startButton.className = "hide";
     paragraph1.className = "hide";
     optionsEl.classList.remove('hide');
-    optionsEl.textContent = "";
+    questionsEl.className = "hide";
+    optionsEl.textContent = "";      // makes H1 go away
+
  
-    
+// question
+      var h1 = document.createElement("H1");
+      h1.style.textAlign = "center";
+      h1.textContent = questions[currentQuestionIndex].question;
+      optionsEl.appendChild(h1);
+
+//answer    
     currentQuestion.options.forEach((answer, index) => {
       var buttonOption = document.createElement("button");
       buttonOption.setAttribute("class", "option-buttons")
@@ -77,116 +83,15 @@ startButton.addEventListener("click", function() {
     }
   
 
+function scorePage() {
+    // when there are no more questions left, head to the score page and stop the timer
+   var finalQuestion = questions[currentQuestionIndex];
+   
+    if(questions > questions.length) {
+        countdown = setInterval(); 
+
+    }
+}
+
+
     startButton.addEventListener("click", startGame);
-
-  
-
-  //START GAME BUTTON PRESS
-//   function startGame(){
-//       //questionsEl.textContent = question1;
-//       questionsEl.textContent = questions[0].question;
-//       startButton.className = "hide";
-//       paragraph1.className = "hide";
-//       optionsEl.classList.remove('hide');
-//       option1.textContent = questions[0].options[0];
-//       option2.textContent = questions[0].options[1];
-//       option3.textContent = questions[0].options[2];
-//       option4.textContent = questions[0].options[3];
-
-//       option1.addEventListener("click", function() { 
-//        // alert('wrong');
-//         //question2();
-//     })
-//       option2.addEventListener("click", function() { 
-//        //   alert('wrong');
-//           //question2();
-//     })
-//       option3.addEventListener("click", function() { 
-//       //    alert('correct')
-//           //question2();
-//     })
-//       option4.addEventListener("click", function() { 
-//       //  alert('wrong');
-//         //question2();
-//     })
-
-//     if (answer === true) {
-//         alert('CORRECT');
-//     }
-//   };
-
-//   function question2() {
-//     questionsEl.textContent = questions[1].question;
-//     option1.textContent = questions[1].options[0];
-//     option2.textContent = questions[1].options[1];
-//     option3.textContent = questions[1].options[2];
-//     option4.textContent = questions[1].options[3];
-//     option1.addEventListener("click", function() { 
-//        // alert('wrong');
-//         question3();
-//     })
-//       option2.addEventListener("click", function() { 
-//         //  alert('wrong');
-//           question3();
-//     })
-//       option3.addEventListener("click", function() { 
-//         //  alert('correct')
-//           question3();
-//     })
-//       option4.addEventListener("click", function() { 
-//       //  alert('wrong');
-//         question3();
-//     })
-//   }
-//   function question3() {
-//     questionsEl.textContent = questions[2].question;
-//     option1.textContent = questions[2].options[0];
-//     option2.textContent = questions[2].options[1];
-//     option3.textContent = questions[2].options[2];
-//     option4.textContent = questions[2].options[3];
-//     option1.addEventListener("click", function() { 
-//       //  alert('wrong');
-//         question4();
-//     })
-//       option2.addEventListener("click", function() { 
-//       //    alert('wrong');
-//           question4();
-//     })
-//       option3.addEventListener("click", function() { 
-//        //   alert('wrong')
-//           question4();
-//     })
-//       option4.addEventListener("click", function() { 
-//       //  alert('correct');
-//         question4();
-//     })
-//   }
-//   function question4() {
-//     questionsEl.textContent = questions[3].question;
-//     option1.textContent = questions[3].options[0];
-//     option2.textContent = questions[3].options[1];
-//     option3.textContent = questions[3].options[2];
-//     option4.textContent = questions[3].options[3];
-//     option1.addEventListener("click", function() { 
-//         //  alert('wrong');
-//           question5();
-//       })
-//         option2.addEventListener("click", function() { 
-//         //    alert('wrong');
-//             question5();
-//       })
-//         option3.addEventListener("click", function() { 
-//          //   alert('correct')
-//             question5();
-//       })
-//         option4.addEventListener("click", function() { 
-//         //  alert('wrong');
-//           question5();
-//       })
-//   }
-//   function question5() {
-//     questionsEl.textContent = questions[4].question;
-//     option1.textContent = questions[4].options[0];
-//     option2.textContent = questions[4].options[1];
-//     option3.textContent = questions[4].options[2];
-//     option4.textContent = questions[4].options[3];
